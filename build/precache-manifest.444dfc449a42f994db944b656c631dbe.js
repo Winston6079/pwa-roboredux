2d71c48934bab9442a39e1f82f0aa44ccf7d75a4
Winston6079 <boook423@gmail.com>
2021-01-29 11:52:26 +0200

    second

---
+++ build/precache-manifest.444dfc449a42f994db944b656c631dbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "812ad8ad6ccac9b59c55a2908ba0e025",
+    "url": "/pwa-roboredux/index.html"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/pwa-roboredux/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c8948b7b1bc9905564ab",
+    "url": "/pwa-roboredux/static/css/main.888bcc31.chunk.css"
+  },
+  {
+    "revision": "b09d20b975196d7d8093",
+    "url": "/pwa-roboredux/static/js/2.277841f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/pwa-roboredux/static/js/2.277841f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8948b7b1bc9905564ab",
+    "url": "/pwa-roboredux/static/js/main.ed443214.chunk.js"
+  },
+  {
+    "revision": "97c91130e6c256a59fb7",
+    "url": "/pwa-roboredux/static/js/runtime-main.8ad26a4a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/pwa-roboredux/static/media/SEGA.12a3db8b.woff"
+  }
+]);
